f785ed1802cfea71c63dd14fc87940dff7686a5e
GearHead87 <hosanulislam87@gmail.com>
2025-06-30 04:24:38 +0600

    Add @vercel/analytics package and integrate Analytics component into layout for enhanced user tracking and insights.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/sonner';
 import { dbConnect } from '@/lib/mongo';
 import MainLayout from '@/components/layout/MainLayout';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,6 +28,7 @@ export default async function RootLayout({
   await dbConnect();
   return (
     <html lang="en">
+      <Analytics />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <MainLayout>{children}</MainLayout>
         <Toaster />
